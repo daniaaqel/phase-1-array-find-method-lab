40a2e5f8065ae0d82753f34cbce8ddd065a3807c
daniaaqel <dania.aqel@hotmail.com>
2022-09-07 14:32:28 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(array){
+    var winner = array.find((element)=>{
+        return element.result === 'W';
+    });
+    
+    if(winner === undefined){
+        return undefined
+     } else {
+        return winner.year;
+     }
+}
